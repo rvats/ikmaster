1894bf2d840990599e3bbcd98f351db33bc966e0
Rahul Vats <vats.rahul@gmail.com>
2021-01-27 15:15:29 -0600

    Updating Folder Structures

---
+++ Python/SearchingAndSorting/Searching/_ListSearchingAlgorithms.py
@@ -0,0 +1,4 @@
+searchingTopics = ["Linear Search", "Binary Search"]
+print("List Of Searching Topics Covered and Practiced: ")
+for topics in searchingTopics:
+    print(topics)
--- Python/_ListSorting.py
+++
@@ -1,4 +0,0 @@
-sortingAndSearchingTopics = ["Linear Search", "Binary Search", "Bubble Sort", "Selection Sort", "Insertion Sort", "Merge Sort", "Heap Sort", 
-                             "Quick Sort", "Counting Sort", "Bucket Sort", "Radix Sort", "Tim Sort", "Tree Sort", "Shell Sort", "Cube Sort"]
-for topics in sortingAndSearchingTopics:
-    print(topics)
