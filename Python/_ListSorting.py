c591ce44bd425378ff536c0cb5e04498a0650bfb
Rahul Vats <vats.rahul@gmail.com>
2021-01-26 21:48:24 -0600

    Updating List of Topics Covered.

---
+++ Python/_ListSorting.py
@@ -0,0 +1,4 @@
+sortingAndSearchingTopics = ["Linear Search", "Binary Search", "Bubble Sort", "Selection Sort", "Insertion Sort", "Merge Sort", "Heap Sort", 
+                             "Quick Sort", "Counting Sort", "Bucket Sort", "Radix Sort", "Tim Sort", "Tree Sort", "Shell Sort", "Cube Sort"]
+for topics in sortingAndSearchingTopics:
+    print(topics)
