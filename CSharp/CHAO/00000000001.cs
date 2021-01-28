e1755947bbd135a4e183a9e44b2be396df949346
Rahul Vats <vats.rahul@gmail.com>
2021-01-28 00:52:53 -0600

    Updating Code

--- CSharp/CHAO/00000000001.cs
+++ CSharp/CHAO/00000000001.cs
@@ -1,24 +1,16 @@
+/***********************************************************************************************************************
+179. Largest Number: https://leetcode.com/problems/largest-number/
+Given a list of non-negative integers nums, arrange them such that they form the largest number.
+Note: The result may be very large, so you need to return a string instead of an integer
+Example 1: Input: nums = [10,2] Output: "210"
+Example 2: Input: nums = [3,30,34,5,9] Output: "9534330"
+Example 3: Input: nums = [1] Output: "1"
+Example 4: Input: nums = [10] Output: "10"
+***********************************************************************************************************************/
 using System;
 
-public static class Solution
+public class Solution
 {
-    private static void Main()
-    {
-        var nums = new int[] { 1, 12, 113 };
-        Console.WriteLine(CalculateLargestString(nums));
-        nums = new int[] { 10, 2 };
-        Console.WriteLine(CalculateLargestString(nums));
-        nums = new int[] { 1, 2, 3, 4, 5 };
-        Console.WriteLine(CalculateLargestString(nums));
-    }
-
-    private static string CalculateLargestString(int[] nums)
-    {
-        Array.Sort(nums, (a, b) => (b + "" + a).CompareTo(a + "" + b));
-        foreach (var num in nums)
-        {
-            Console.WriteLine(num);
-        }
-        return nums[0] == 0 ? "0" : string.Join("", nums);
-    }
+    Array.Sort(nums, (a, b) => (b + "" + a).CompareTo(a + "" + b));
+    return nums[0] == 0 ? "0" : string.Join("", nums);
 }
