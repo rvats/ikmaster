f1d1ed408c0c2e0d09078d5b23d152ac45be578a
Rahul Vats <vats.rahul@gmail.com>
2021-01-27 21:51:46 -0600

    Adding Shell Sort

--- Python/SearchingAndSorting/Sorting/12ShellSort.py
+++ Python/SearchingAndSorting/Sorting/12ShellSort.py
@@ -1,51 +1,44 @@
+import random
 class Sorting:
-    # Bucket sort is mainly useful when input is uniformly distributed over a range. For example, consider the following problem.
-    # Sort a large set of floating point numbers which are in range from 0.0 to 1.0 and are uniformly distributed across the range. How do we sort the numbers efficiently?
-    # A simple way is to apply a comparison based sorting algorithm. The lower bound for Comparison based sorting algorithm (Merge Sort, Heap Sort, Quick-Sort .. etc) is Ω(n Log n), i.e., they cannot do better than nLogn. Can we sort the array in linear time? Counting sort can not be applied here as we use keys as index in counting sort. Here keys are floating point numbers.
-    # The idea is to use bucket sort. Following is bucket algorithm. bucketSort(arr[], n)
-    # 1) Create n empty buckets (Or lists).
-    # 2) Do following for every array element arr[i].
-        # a) Insert arr[i] into bucket[n*array[i]]
-    # 3) Sort individual buckets using insertion sort.
-    # 4) Concatenate all sorted buckets.
+    # ShellSort is mainly a variation of Insertion Sort. In insertion sort, we move elements only one position ahead. When an element has to be moved far ahead, many movements are involved. The idea of shellSort is to allow exchange of far items. In shellSort, we make the array h-sorted for a large value of h. We keep reducing the value of h until it becomes 1. An array is said to be h-sorted if all sublists of every h’th element is sorted.
 
-    #@insertionSort
-    def insertionSort(self, bucket): 
-        for i in range(1, len(bucket)): 
-            up = bucket[i] 
-            j = i - 1
-            while j >= 0 and bucket[j] > up:  
-                bucket[j + 1] = bucket[j] 
-                j -= 1
-            bucket[j + 1] = up      
-        return bucket      
-              
-    #@bucketSort
-    def bucketSort(self, numbers): 
-        bucket = [] 
-        slot_num = len(numbers)-1 
-        for i in range(slot_num): 
-            bucket.append([]) 
-          
-        # Put array elements in different buckets  
-        for j in numbers: 
-            index_b = int(slot_num * j)  
-            bucket[index_b].append(j) 
-      
-        # Sort individual buckets  
-        for i in range(slot_num): 
-            bucket[i] = self.insertionSort(bucket[i]) 
-          
-        # concatenate the result 
-        k = 0
-        for i in range(slot_num): 
-            for j in range(len(bucket[i])): 
-                numbers[k] = bucket[i][j] 
-                k += 1
-        return numbers
+    #@shellSort
+    def shellSort(self, arr): 
+        # Start with a big gap, then reduce the gap 
+        n = len(arr) 
+        gap = n//2
+  
+        # Do a gapped insertion sort for this gap size. 
+        # The first gap elements a[0..gap-1] are already in gapped  
+        # order keep adding one more element until the entire array 
+        # is gap sorted 
+        while gap > 0: 
+  
+            for i in range(gap,n): 
+  
+                # add a[i] to the elements that have been gap sorted 
+                # save a[i] in temp and make a hole at position i 
+                temp = arr[i] 
+  
+                # shift earlier gap-sorted elements up until the correct 
+                # location for a[i] is found 
+                j = i 
+                while  j >= gap and arr[j-gap] >temp: 
+                    arr[j] = arr[j-gap] 
+                    j -= gap 
+  
+                # put temp (the original a[i]) in its correct location 
+                arr[j] = temp 
+            gap //= 2
+        return arr
 
 if __name__ == '__main__':
-    print("Bucket Sort Demo")
-    numbers = [0.897, 0.565, 0.656, 
-     0.1234, 0.665, 0.3434] 
-    print(Sorting().bucketSort(numbers))
+    print("Shell Sort Demo")
+    numbers = []
+    for _ in range(10):
+        n = random.randint(0,100) # whatever your range of random numbers is
+        numbers.append(n)
+    print("Numbers before Sorting: ")
+    print(numbers)
+    print("Numbers after Sorting: ")
+    print(Sorting().shellSort(numbers))
