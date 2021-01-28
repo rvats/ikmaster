93ee03b15fdfc4e612dd81600b87dd22ab1bfacf
Rahul Vats <vats.rahul@gmail.com>
2021-01-27 20:28:42 -0600

    Updating QuickSort and CountSort

--- Python/SearchingAndSorting/Sorting/06QuickSort.py
+++ Python/SearchingAndSorting/Sorting/06QuickSort.py
@@ -1,51 +1,47 @@
+# imports random
+import random
 class Sorting:
-    # Bucket sort is mainly useful when input is uniformly distributed over a range. For example, consider the following problem.
-    # Sort a large set of floating point numbers which are in range from 0.0 to 1.0 and are uniformly distributed across the range. How do we sort the numbers efficiently?
-    # A simple way is to apply a comparison based sorting algorithm. The lower bound for Comparison based sorting algorithm (Merge Sort, Heap Sort, Quick-Sort .. etc) is Ω(n Log n), i.e., they cannot do better than nLogn. Can we sort the array in linear time? Counting sort can not be applied here as we use keys as index in counting sort. Here keys are floating point numbers.
-    # The idea is to use bucket sort. Following is bucket algorithm. bucketSort(arr[], n)
-    # 1) Create n empty buckets (Or lists).
-    # 2) Do following for every array element arr[i].
-        # a) Insert arr[i] into bucket[n*array[i]]
-    # 3) Sort individual buckets using insertion sort.
-    # 4) Concatenate all sorted buckets.
+    # Like Merge Sort, QuickSort is a Divide and Conquer algorithm. It picks an element as pivot and partitions the given array around the picked pivot. There are many different versions of quickSort that pick pivot in different ways.
+    # Always pick first element as pivot.
+    # Always pick last element as pivot (implemented below)
+    # Pick a random element as pivot.
+    # Pick median as pivot.
+    # The key process in quickSort is partition(). 
+    # /* low  --> Starting index,  high  --> Ending index */
+    # quickSort(numbers[], low, high):
+    #   if (low < high)
+    #   /* pi is partitioning index, numbers[pi] is now at right place */
+    #       pi = partition(numbers, low, high);
+    #       quickSort(numbers, low, pi - 1);  // Before pi
+    #       quickSort(numbers, pi + 1, high); // After pi
 
-    #@insertionSort
-    def insertionSort(self, bucket): 
-        for i in range(1, len(bucket)): 
-            up = bucket[i] 
-            j = i - 1
-            while j >= 0 and bucket[j] > up:  
-                bucket[j + 1] = bucket[j] 
-                j -= 1
-            bucket[j + 1] = up      
-        return bucket      
-              
-    #@bucketSort
-    def bucketSort(self, numbers): 
-        bucket = [] 
-        slot_num = len(numbers)-1 
-        for i in range(slot_num): 
-            bucket.append([]) 
-          
-        # Put array elements in different buckets  
-        for j in numbers: 
-            index_b = int(slot_num * j)  
-            bucket[index_b].append(j) 
-      
-        # Sort individual buckets  
-        for i in range(slot_num): 
-            bucket[i] = self.insertionSort(bucket[i]) 
-          
-        # concatenate the result 
-        k = 0
-        for i in range(slot_num): 
-            for j in range(len(bucket[i])): 
-                numbers[k] = bucket[i][j] 
-                k += 1
-        return numbers
+    #@quickSort
+    def quickSort(self, numbers, low, high):
+        if (low < high):
+            pi = self.partition(numbers, low, high)
+            self.quickSort(numbers, low, pi - 1)
+            self.quickSort(numbers, pi + 1, high)
+
+    # This function takes last element as pivot, places 
+    # the pivot element at its correct position in sorted 
+    # array, and places all smaller (smaller than pivot) 
+    # to left of pivot and all greater elements to right 
+    # of pivot 
+    def partition(self, numbers, low, high): 
+        i = ( low-1 )         # index of smaller element 
+        # randint generates a random integar between the first parameter and the second
+        pivot = numbers[high]     # pivot random.randint(low, high)
+        for j in range(low , high): 
+            # If current element is smaller than the pivot 
+            if numbers[j] < pivot: 
+                # increment index of smaller element 
+                i = i+1 
+                numbers[i],numbers[j] = numbers[j],numbers[i] 
+        numbers[i+1],numbers[high] = numbers[high],numbers[i+1] 
+        return ( i+1 ) 
 
 if __name__ == '__main__':
-    print("Bucket Sort Demo")
-    numbers = [0.897, 0.565, 0.656, 
-     0.1234, 0.665, 0.3434] 
-    print(Sorting().bucketSort(numbers))
+    print("Quick Sort Demo")
+    numbers = [8971, 9565, 8656, 1234, 665, 3434] 
+    Sorting().quickSort(numbers,0,len(numbers)-1)
+    print(numbers)
