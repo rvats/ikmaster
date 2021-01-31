ce43267a3d0fb137486a5ce1642da793a39c8dd9
Rahul Vats <vats.rahul@gmail.com>
2021-01-31 11:24:09 -0600

    Combination Or Subsets of Set

---
+++ Python/Recursion/CombinationStrings.py
@@ -0,0 +1,28 @@
+# Combination Helper
+def combinationHelper(dataCollected, inputSet):
+    inputSetLength = len(inputSet)
+    if inputSetLength == 0:
+        print(dataCollected)
+    else:
+        for i in range(inputSetLength):
+            combinationHelper(dataCollected, inputSet[1:])
+            dataCollected.append(inputSet[0])
+            combinationHelper(dataCollected, inputSet[1:])
+#inputSet = ["a","b","c","d"]
+#inputSet = ["abcd"]
+#inputSet = "abcd"
+#combinationHelper([], inputSet)
+
+# Function to create combinations without itertools 
+def combinationGenerator(dataCollected, inputSet): 
+    inputSetLength = len(inputSet)
+    if inputSetLength == 0: 
+        print(dataCollected)
+    for i in range(inputSetLength): 
+        combinationGenerator(dataCollected, inputSet[i + 1:])
+        dataCollected.append([inputSet[i]]) 
+        combinationGenerator(dataCollected, inputSet[i + 1:])
+# Driver code 
+if __name__=="__main__": 
+    arr ="abc"
+    combinationGenerator([], [x for x in arr])
