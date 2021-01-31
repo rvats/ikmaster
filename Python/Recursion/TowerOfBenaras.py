8dae80a34d8c5dfc75b917de9c3b2fd15b306d46
Rahul Vats <vats.rahul@gmail.com>
2021-01-30 19:11:50 -0600

    Tower of Benaras with Move Counts

--- Python/Recursion/TowerOfBenaras.py
+++ Python/Recursion/TowerOfBenaras.py
@@ -1,12 +1,16 @@
 # Recursive Python function to solve tower of benaras
+numberOfMoves = 0
 def towerOfBenaras(n , source, destination, auxilliary): 
+    global numberOfMoves
     if n==1: 
-        print("Move disk 1 from source ", source, " to destination ", destination)
+        numberOfMoves+=1
+        print("Move disk 1 from source ", source, " to destination ", destination, "Moves Count: ", numberOfMoves)
         return
     towerOfBenaras(n-1, source, auxilliary, destination) 
-    print("Move disk ", n," from source ", source, " to destination ", destination)
+    numberOfMoves+=1
+    print("Move disk ", n," from source ", source, " to destination ", destination, "Moves Count: ", numberOfMoves)
     towerOfBenaras(n-1, auxilliary, destination, source) 
           
 # Driver code 
-n = 6
+n = 3
 towerOfBenaras(n,'A','B','C')
