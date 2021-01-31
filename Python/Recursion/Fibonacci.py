c24a9b8cd070de33030d40b0353e67f428d60eb0
Rahul Vats <vats.rahul@gmail.com>
2021-01-30 18:01:50 -0600

    Updating Code of Fibonacci Sequence

--- Python/Recursion/Fibonacci.py
+++ Python/Recursion/Fibonacci.py
@@ -19,6 +19,8 @@ private int fibTopDown(int n) {
     if (n <= 1) return 1;
     return fibTopDown(n - 1) + fibTopDown(n - 2);
 }
+Resources: 
+https://towardsdatascience.com/memoization-in-python-57c0a738179a
 '''
 # Fibonacci Series No Optimization
 # Time Complexity = O(n) Space Complexity = O(n)
@@ -27,39 +29,43 @@ def fibonacciNaive(number):
         return 1
     else:
         return fibonacciNaive(number-1) + fibonacciNaive(number-2)
+for i in range(1, 36):
+     print("fibonacciNaive({}) = ".format(i), fibonacciNaive(i))
 
-print(fibonacciNaive(25))
 
+fibonacci_cache = {}
 # Fibonacci Series using Dynamic Programming using Memoization 1
 # Time Complexity = O(n) Space Complexity = O(n)
-def fibonacci_memo(number):
+def fibonacciMemo(number):
     if number in fibonacci_cache:
         return fibonacci_cache[number]
     if number <= 2:
         return 1
     else:           
-        value =  fibonacci_memo(number-1) + fibonacci_memo(number-2)
+        value =  fibonacciMemo(number-1) + fibonacciMemo(number-2)
     fibonacci_cache[number] = value
     return value
+for i in range(1, 1003):
+     print("fibonacciMemo({}) = ".format(i), fibonacciMemo(i))
 
 # Fibonacci Series using Dynamic Programming using Memoization 2
 # Time Complexity = O(n) Space Complexity = O(n)
-@lru_cache(maxsize = 1000)
-def fibonacci_cache(number):
+@lru_cache(maxsize = 1003)
+def fibonacciCache(number):
     if number <= 2:
         return 1
     else:
-        return fibonacci_cache(number-1) + fibonacci_cache(number-2)
-for i in range(1, 201):
-     print("fibonacci({}) = ".format(i), fibonacci_cache(i))
+        return fibonacciCache(number-1) + fibonacciCache(number-2)
+for i in range(1, 1003):
+     print("fibonacciCache({}) = ".format(i), fibonacciCache(i))
 
-# Fibonacci Series using Dynamic Programming using Memoization 3
+# Fibonacci Series using Iterative Approach using Tabularization 3
 # Time Complexity = O(n)
-def fibonacci(n):  
+def fibonacciIterative(n):  
     # Taking 1st two fibonacci nubers as 0 and 1  
     f = [0, 1, 1]  
     for i in range(3, n+1): 
         f.append(f[i-1] + f[i-2]) 
     return f 
-for index,number in enumerate(fibonacci(100)):
-    print("fibonacci({}) = ".format(index), number)
+for index,number in enumerate(fibonacciIterative(1003)):
+    print("fibonacciIterative({}) = ".format(index), number)
