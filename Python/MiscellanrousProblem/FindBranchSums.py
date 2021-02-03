e996421c8b495a2be66a0472eb59328401dde17d
Rahul Vats <vats.rahul@gmail.com>
2021-02-03 03:15:49 -0600

    Correcting The Swap of the code

--- Python/MiscellanrousProblem/FindBranchSums.py
+++ Python/MiscellanrousProblem/FindBranchSums.py
@@ -1,38 +1,33 @@
-# This is the class of the input root. Do not edit it.
+﻿# This is the class of the input root. Do not edit it.
 class BinaryTree:
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
 
-def nodeDepths(root):
-    nodeDepthsSum, currentNodeDepth = 0, 0
-    def nodeDepthsSumHelper(root, currentNodeDepth, nodeDepthsSum):
+def branchSums(root):
+    branchSumsCollection = []
+    def branchSumsHelper(root, branchSumsCollection, currentBranchSum):
         if root.left is None and root.right is None:
-            nodeDepthsSum = nodeDepthsSum + currentNodeDepth
-            print(root.value, currentNodeDepth, nodeDepthsSum)
-            return nodeDepthsSum
+            branchSumsCollection.append(currentBranchSum+root.value)
         else:
             if(root.left is not None):
-                nodeDepthsSum = nodeDepthsSumHelper(root.left,currentNodeDepth+1,nodeDepthsSum + currentNodeDepth)
-                print(root.value, currentNodeDepth, nodeDepthsSum)
+                branchSumsHelper(root.left,branchSumsCollection,currentBranchSum+root.value)
             if(root.right is not None):
-                nodeDepthsSum = nodeDepthsSumHelper(root.right,currentNodeDepth+1,nodeDepthsSum + currentNodeDepth)
-                print(root.value, currentNodeDepth, nodeDepthsSum)
-        return nodeDepthsSum
-    nodeDepthsSum = nodeDepthsSumHelper(root,0,0)
-    print(root.value, currentNodeDepth, nodeDepthsSum)
-    return nodeDepthsSum
+                branchSumsHelper(root.right,branchSumsCollection,currentBranchSum+root.value)
+        return branchSumsCollection
+    branchSumsHelper(root,branchSumsCollection,0)
+    return branchSumsCollection
 
-root = BinaryTree(1) # Depth = 0
-root.left = BinaryTree(2) # Depth = 1
-root.right = BinaryTree(3) # Depth = 1
-root.left.left = BinaryTree(4) # Depth = 2
-root.left.right = BinaryTree(5) # Depth = 2
-#root.right.left = BinaryTree(6) # Depth = 2
-#root.right.right = BinaryTree(7) # Depth = 2
-#root.left.left.left = BinaryTree(8)  # Depth = 3
-#root.left.left.right = BinaryTree(9) # Depth = 3
-#root.left.right.left = BinaryTree(10) # Depth = 3
+root = BinaryTree(1)
+root.left = BinaryTree(2)
+root.right = BinaryTree(3)
+root.left.left = BinaryTree(4)
+root.left.right = BinaryTree(5)
+root.right.left = BinaryTree(6)
+root.right.right = BinaryTree(7)
+root.left.left.left = BinaryTree(8)
+root.left.left.right = BinaryTree(9)
+root.left.right.left = BinaryTree(10)
 
-print(nodeDepths(root))
+print(branchSums(root))
--- Python/MiscellanrousProblem/FindSumOfNodeDepths.py
+++ Python/MiscellanrousProblem/FindSumOfNodeDepths.py
@@ -5,29 +5,37 @@ class BinaryTree:
         self.left = None
         self.right = None
 
-def branchSums(root):
-    branchSumsCollection = []
-    def branchSumsHelper(root, branchSumsCollection, currentBranchSum):
-        if root.left is None and root.right is None:
-            branchSumsCollection.append(currentBranchSum+root.value)
-        else:
-            if(root.left is not None):
-                branchSumsHelper(root.left,branchSumsCollection,currentBranchSum+root.value)
-            if(root.right is not None):
-                branchSumsHelper(root.right,branchSumsCollection,currentBranchSum+root.value)
-        return branchSumsCollection
-    branchSumsHelper(root,branchSumsCollection,0)
-    return branchSumsCollection
+# Average case: when the tree is balanced
+# O(n) time | O(h) space - where n is the number of nodes in
+# the Binary Tree and h is the height of the Binary Tree
+def nodeDepths(root, depth=0):
+    if root is None:
+        return 0
+    print(root.value, depth)
+    return depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1)
 
-root = BinaryTree(1)
-root.left = BinaryTree(2)
-root.right = BinaryTree(3)
-root.left.left = BinaryTree(4)
-root.left.right = BinaryTree(5)
-root.right.left = BinaryTree(6)
-root.right.right = BinaryTree(7)
-root.left.left.left = BinaryTree(8)
-root.left.left.right = BinaryTree(9)
-root.left.right.left = BinaryTree(10)
+def nodeDepthsIterative (root): 
+	sumOfDepths = 0
+	stack = [{"node": root, "depth": 0}]
+	while len(stack) > 0:
+		nodeInfo = stack.pop()
+		node, depth = nodeInfo["node"], nodeInfo["depth"]
+		if node is None:
+			continue
+		sumOfDepths+=depth
+		stack.append({"node": node.left, "depth": depth+1})
+		stack.append({"node": node.right, "depth": depth+1})
+	return sumOfDepths
 
-print(branchSums(root))
+root = BinaryTree(1) # Depth = 0
+root.left = BinaryTree(2) # Depth = 1
+root.right = BinaryTree(3) # Depth = 1
+root.left.left = BinaryTree(4) # Depth = 2
+root.left.right = BinaryTree(5) # Depth = 2
+root.right.left = BinaryTree(6) # Depth = 2
+root.right.right = BinaryTree(7) # Depth = 2
+root.left.left.left = BinaryTree(8)  # Depth = 3
+root.left.left.right = BinaryTree(9) # Depth = 3
+root.left.right.left = BinaryTree(10) # Depth = 3
+
+print(nodeDepths(root))
