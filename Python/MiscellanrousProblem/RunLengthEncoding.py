2d6ace076ab250bb5ffb0b6cee1d1c4576d7182a
Rahul Vats <vats.rahul@gmail.com>
2021-02-04 14:28:21 -0600

    Using additional Space

--- Python/MiscellanrousProblem/RunLengthEncoding.py
+++ Python/MiscellanrousProblem/RunLengthEncoding.py
@@ -1,17 +1,18 @@
 def runLengthEncoding(string):
-    result = ""
-    charCount = 0
-    for index in range(len(string)-1):
-        if charCount == 8:
-            result+=str(charCount+1)+string[index]
-            charCount = -1
-        if string[index]!=string[index+1]:
-            result+=str(charCount+1)+string[index]
-            charCount=0
-        else:
-            charCount+=1
-    result+=str(charCount+1)+string[len(string)-1]
-    return result
+	encodedStringCharacters = []
+	currentRunLength = 1
+	for i in range(1, len(string)):
+		currentCharacter = string[i]
+		previousCharacter = string[i-1]
+		if currentCharacter != previousCharacter or currentRunLength ==9:
+			encodedStringCharacters.append(str(currentRunLength))
+			encodedStringCharacters.append(previousCharacter)
+			currentRunLength = 0
+		currentRunLength+=1
+	encodedStringCharacters.append(str(currentRunLength))
+	encodedStringCharacters.append(string[len(string)-1])
+	return "".join(encodedStringCharacters)
+
 result = runLengthEncoding("AAAAAAAAAAAAABBCCCCDD")
 print("Input: AAAAAAAAAAAAABBCCCCDD")
 print("Output: ", result)
