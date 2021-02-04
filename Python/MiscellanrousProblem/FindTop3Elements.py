0828780a2d1069aaa90469b1c160e03be5ecd806
Rahul Vats <vats.rahul@gmail.com>
2021-02-04 15:34:11 -0600

    Updating the condition

--- Python/MiscellanrousProblem/FindTop3Elements.py
+++ Python/MiscellanrousProblem/FindTop3Elements.py
@@ -23,9 +23,9 @@ def findThreeLargestNumbers(array):
 def updateLargest(threeLargest, num):
 	if threeLargest[2] is None or num > threeLargest[2]:
 		shiftAndUpdate(threeLargest, num, 2)
-	if threeLargest[1] is None or num > threeLargest[1]:
+	elif threeLargest[1] is None or num > threeLargest[1]:
 		shiftAndUpdate(threeLargest, num, 1)
-	if threeLargest[0] is None or num > threeLargest[0]:
+	elif threeLargest[0] is None or num > threeLargest[0]:
 		shiftAndUpdate(threeLargest, num, 0)
 		
 def shiftAndUpdate(array, num, idx):
