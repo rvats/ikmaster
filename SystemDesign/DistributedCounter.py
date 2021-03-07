b8d65158d2dc50ac034cd4176908d8ff23517018
Rahul Vats <vats.rahul@gmail.com>
2021-03-07 00:38:56 -0600

    Distributed Counter Notes

--- SystemDesign/DistributedCounter.py
+++ SystemDesign/DistributedCounter.py
@@ -1,5 +1,5 @@
 import time
-def distributedCounter():
+def counter():
     start = time.time()
     counter = 0
     while counter < 1000000001:
@@ -7,4 +7,11 @@ def distributedCounter():
         print(counter)
     end = time.time()
     print(end - start)
+
+def distributedCounter()(self, doc_ref):
+    """Increment a randomly picked shard."""
+    doc_id = random.randint(0, self._num_shards - 1)
+
+    shard_ref = doc_ref.collection("shards").document(str(doc_id))
+    return shard_ref.update({"count": firestore.Increment(1)})
 distributedCounter()
